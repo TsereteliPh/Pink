12361940c806546efa4786c62436102cc36459cc
TsereteliPh <filippok01001@gmail.com>
2022-08-17 20:28:17 +0300

    Установлено несколько плагинов галп

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,37 @@
 const gulp = require('gulp');
 const del = require('del');
+const less = require('gulp-less');
+const sourcemap = require('gulp-sourcemaps');
+
+const paths = {
+    styles: {
+        src: 'src/less/**/*.less',
+        dest: 'dist/css/'
+    },
+    scripts: {
+        src: 'src/js/**/*.js',
+        dest: 'dist/scripts/'
+    },
+    img: {
+        src: 'src/img/**',
+        dest: 'dist/img/'
+    }
+};
+
+//Функция для очистки финальной папки
 
 function clean() {
     return del(['dist/**', '!dist/html/**']);
+}
+
+//Функция обработки стилей
+
+function styles() {
+    return gulp.src(paths.styles.src)
+    .pipe(sourcemap.init())
+    .pipe(less())
+    
+
 }
  
 exports.clean = clean;
